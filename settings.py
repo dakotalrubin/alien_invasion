6295207a465d5071897448f51fdf3c6fa36be022
dlrubin <dlrubin@ucsc.edu>
2023-08-04 08:38:15 -0700

    Screen boundaries for left and right movement

--- settings.py
+++ settings.py
@@ -12,3 +12,6 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (107, 73, 132)
+
+        # Ship settings
+        self.ship_speed = 1.5
--- ship.py
+++ ship.py
@@ -11,6 +11,7 @@ class Ship:
     def __init__(self, game):
         self.screen = game.screen
         self.screen_rect = game.screen.get_rect()
+        self.settings = game.settings
 
         # Load the ship image and get its rectangle (sprite boundaries)
         self.image = pygame.image.load("images/ships/ship_002.png")
@@ -19,16 +20,24 @@ class Ship:
         # Start each new ship at the bottom-center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Store a float for the ship's horizontal position
+        self.x = float(self.rect.x)
+
         # Movement flags (start with a ship that's not moving)
         self.moving_right = False
         self.moving_left = False
 
     """Update the ship's position based on movement flags."""
     def update(self):
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+
+        # Update the ship's x value
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x
+        self.rect.x = self.x
 
     """Draw the ship at its current location."""
     def blitme(self):
